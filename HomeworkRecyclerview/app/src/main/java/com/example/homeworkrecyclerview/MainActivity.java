d3dd4945a2a61286ef41c0c84b409e6b298250a4
AnantaNeupane <neupaneananta18@gmail.com>
2021-03-21 10:12:26 +0545

    made a layout to display food name and ingrediants

--- HomeworkRecyclerview/app/src/main/java/com/example/homeworkrecyclerview/MainActivity.java
+++ HomeworkRecyclerview/app/src/main/java/com/example/homeworkrecyclerview/MainActivity.java
@@ -1,19 +1,19 @@
-package com.example.homeworkrecyclerview;
+package com.example.RecycleViewHomework;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinkedList<Recipe> recipes;
+    private RecyclerView rcView;
+    private RecipeAdapter adapter;
+    public static final String EXTRA_REPLY = "com.example.RecycleViewHomework.extra.REPLY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,35 +22,20 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
+        recipes = DataProvider.getRecipes();
+
+        rcView = findViewById(R.id.recyclerview);
+
+        adapter = new RecipeAdapter(this, recipes);
+
+        rcView.setAdapter(adapter);
+
+        rcView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 }
